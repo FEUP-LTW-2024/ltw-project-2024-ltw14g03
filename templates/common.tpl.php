3bdd000142d5d5f50b2a31ffed260af7b1e829fe
Rui Borges <ruiborges206@gmail.com>
2024-04-22 21:39:09 +0100

    Added image and username to the menu bar when logged in

--- templates/common.tpl.php
+++ templates/common.tpl.php
@@ -28,7 +28,13 @@
                 <li><a href="#">Sell</a></li>
                 <li><a href="#">Profile</a></li>
                 <?php if(isset($_SESSION['username'])) { ?>
-                  <li><a href="../pages/login.php">LOGGED IN</a></li>
+                  <li class="dropdown"><img class="profileImageBar" src="https://via.placeholder.com/150"><a href="../pages/login.php"><?php echo  $_SESSION['username']?></a>
+                  <div class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                  </div>
+                  </li>
                 <?php } else { ?>
                   <li><a href="../pages/login.php">Sign In/Sign Up</a></li>
                 <?php } ?>
