eff18ea241a73f3f61c4018188bb63b71296c28e
GuilhermeGuerra2004 <147815799+GuilhermeGuerra2004@users.noreply.github.com>
2024-05-07 14:58:38 +0100

    updated

--- templates/common.tpl.php
+++ templates/common.tpl.php
@@ -16,17 +16,17 @@
     <link rel="stylesheet" href="../assets/style/layout.css">
     <link rel="stylesheet" href="../assets/style/responsive.css">
     <script src="../scripts/logout.js"></script>
-    <?php if($_SERVER['REQUEST_URI']=="/pages/register.php"){?>
+    <?php if($_SERVER['REQUEST_URI']=="/ltw-project-2024-ltw14g03/pages/register.php"){?>
       <script src="../scripts/validatePassword.js"></script>
     <?php } ?>
-    <?php if($_SERVER['REQUEST_URI']=="/pages/add_sell_order.php"){?>
+    <?php if($_SERVER['REQUEST_URI']=="/ltw-project-2024-ltw14g03/pages/add_sell_order.php"){?>
       <script src="../scripts/sellOrder.js"></script>
       <script src="../scripts/addSellOrderWithImage.js"></script>
     <?php } ?>
-    <?php if($_SERVER['REQUEST_URI']=="/pages/profile.php"){?>
+    <?php if($_SERVER['REQUEST_URI']=="/ltw-project-2024-ltw14g03/pages/profile.php"){?>
       <script src="../scripts/editProfile.js"></script>
     <?php } ?>
-    <?php if($_SERVER['REQUEST_URI']=="/pages/index.php"){?>
+    <?php if($_SERVER['REQUEST_URI']=="/ltw-project-2024-ltw14g03/pages/index.php"){?>
       <script src="../scripts/showSellOrders.js"></script>
       <script src="../scripts/showSellOrdersFiltered.js"></script>
     <?php } ?>
